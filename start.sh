b64b813071c89271194ad015d0f723c23badfb05
hongfanmeng <hongfanmeng@gmail.com>
2021-05-13 01:55:49 -0400

    feat: virtual environment

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+echo "Check Python installation..."
+python3 --version
+
+echo "Check virtual environment..."
+
+if [ ! -d "venv" ]; then
+    echo "Create virtual environment..."
+    python3 -m venv venv
+    echo "Activate virtual environment..."
+    . venv/bin/activate
+    echo "Install dependencies"
+    python -m pip install --upgrade pip
+    python -m pip install -r requirements.txt
+else
+    echo "Activate virtual environment..."
+    . venv/bin/activate
+fi
+
+python manage.py runserver
